12a75ba74eb01f3a82e4fb7ad46163301f76815f
DESKTOP-NT13LPL\ccs_win10_17 <markey.wong@gmail.com>
2023-02-05 17:58:49 +0700

    imprement html css find item page

--- src/components/randomItem/RandomItem.js
+++ src/components/randomItem/RandomItem.js
@@ -1,3 +1,4 @@
+import "./RandomItem.css"
 import { Navigate, Route, Routes } from "react-router-dom";
 import LayoutPage from "../../layout/LayoutPage";
 import SelectButtom from "../../util/selectButtom/SelectButtom";
@@ -10,17 +11,22 @@ function RandomItem() {
           <Route
             path="stick"
             element={
-              <>
-                <h3>Stick</h3>
-              </>
+              <div className="box-item">
+                <img className="box" src='/box.png' alt="box-img" />
+                <h2 className="box-text">Common50%  Uncommon25%    Rare13%  UltraRare7%</h2>
+                <p className="box-text random-buttom">500 Token</p>
+              </div>
             }
           />
           <Route
             path="bullet"
             element={
-              <>
-                <h3>Bullet</h3>
-              </>
+              <div className="box-item">
+                <img className="box" src='/box.png' alt="box-img"/>
+                <h2 className="box-text">Uncommon60%    Rare30%  UltraRare10%</h2>
+                <p className="box-text random-buttom">300 Token</p>
+              </div>
+
             }
           />
           <Route path="/" element={<Navigate to="stick" replace={true} />} />
