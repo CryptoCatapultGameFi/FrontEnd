15afc161747e02c74a556d7bc02bb041b62e1501
Ing140943 <skladeeing@gmail.com>
2023-02-11 15:03:03 +0700

    🎨 Add button to play unity game

--- src/components/play/Play.js
+++ src/components/play/Play.js
@@ -1,17 +1,10 @@
 import LayoutPage from "../../layout/LayoutPage";
-import { Unity, useUnityContext } from "react-unity-webgl";
 import { WalletContext } from "../../App";
 import React, { useContext } from "react";
 import "./Play.css";
 
 function Play() {
   const { account } = useContext(WalletContext);
-  const { unityProvider } = useUnityContext({
-    loaderUrl: "Build/BuildGl2.loader.js",
-    dataUrl: "Build/BuildGl2.data",
-    frameworkUrl: "Build/BuildGl2.framework.js",
-    codeUrl: "Build/BuildGl2.wasm",
-  });
   if (account === null) {
     return (
       <LayoutPage>
@@ -21,11 +14,7 @@ function Play() {
   }
   else {
     return (
-      <Unity
-        unityProvider={unityProvider}
-        className="unityComponent"
-        // style={{ width: 1200, height: 600}}
-      />
+      <button type="button" class="btn btn-info entryUnity"><a href="https://cryptocatapult-f75a8.web.app/" >Let's Go</a></button>
     );
   }
 
